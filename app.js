f36cb0928491be56f7ef933bd1ae91c3455c83a8
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-09-13 10:45:58 +0530

    d

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
 const express = require("express");
 const app = express();
-const { createServer } = require("http");
-const path = require("path");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const http = require("http").createServer(app);
 require("dotenv").config();
-
-const server = createServer(app);
-const io = new Server(server, {
+const socketIo = require("socket.io")(http, {
   cors: {
     origin: process.env.VITE_PUBLIC_HOST,
     methods: ["GET", "POST"],
     credentials: true,
   },
-  rejectUnauthorized: false,
 });
 
-app.use(express.json());
-app.use(cors());
+
 const markers = {};
 
-io.on("connection", (socket) => {
-  console.log("connection", socket.id, 23);
-  socket.on("disconnect", () => {
-    io.emit("user-disconnect", socket.id);
-  });
+http.listen(4000, () => {
+  socketIo.on("connection", (socket) => {
+    console.log("connection", socket.id, 23);
+    socket.on("disconnect", () => {
+      socket.emit("user-disconnect", socket.id);
+    });
 
-  socket.on("user-disconnect", (id) => {
-    if (markers[id]) {
-      delete markers[id];
-    }
-  });
+    socket.on("user-disconnect", (id) => {
+      if (markers[id]) {
+        delete markers[id];
+      }
+    });
 
-  socket.on("send-location", (data) => {
-    const { latitude, longitude } = data;
-    if (!markers[socket.id]) {
-      markers[socket.id] = { latitude, longitude };
-    }
-    socket.emit("All-location", markers);
+    socket.on("send-location", (data) => {
+      const { latitude, longitude } = data;
+      if (!markers[socket.id]) {
+        markers[socket.id] = { latitude, longitude };
+      }
+      socket.emit("All-location", markers);
+    });
   });
 });
-
-app.get("/", (req, res) => {
-  res.send({ markers });
-});
-
-server.listen(4000);
