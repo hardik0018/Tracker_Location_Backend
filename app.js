0d22dbd3f7cd9236ae80f3afcff4b07de5ca132d
hardik0018 <141490030+hardik0018@users.noreply.github.com>
2024-10-21 17:18:23 +0530

    fix error

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 const express = require("express");
 const app = express();
-const https = require("https").createServer(app);
 require("dotenv").config();
-const socketIo = require("socket.io")(https, {
+const socketIo = require("socket.io");
+
+const markers = {};
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server Listening at PORT - ${PORT}`);
+});
+
+const io = new socketIo.Server(server, {
   cors: {
     origin: process.env.VITE_PUBLIC_HOST,
-    methods: ["GET", "POST"],
-    credentials: true,
   },
 });
 
-const markers = {};
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    socket.emit("user-disconnect", socket.id);
+  });
 
-https.listen(4000, () => {
-  socketIo.on("connection", (socket) => {
-    console.log("connectison", socket.id, 23);
-    socket.on("disconnect", () => {
-      socket.emit("user-disconnect", socket.id);
-    });
+  socket.on("user-disconnect", (id) => {
+    if (markers[id]) {
+      delete markers[id];
+    }
+  });
 
-    socket.on("user-disconnect", (id) => {
-      if (markers[id]) {
-        delete markers[id];
-      }
-    });
-
-    socket.on("send-location", (data) => {
-      const { latitude, longitude } = data;
-      if (!markers[socket.id]) {
-        markers[socket.id] = { latitude, longitude };
-      }
-      socket.emit("All-location", markers);
-    });
+  socket.on("send-location", (data) => {
+    const { latitude, longitude } = data;
+    if (!markers[socket.id]) {
+      markers[socket.id] = { latitude, longitude };
+    }
+    socket.emit("All-location", markers);
   });
 });
